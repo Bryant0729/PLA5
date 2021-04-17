53585afab867b658324524064383452428131f1c
Bryant0729 <79735967+Bryant0729@users.noreply.github.com>
2021-04-17 16:18:28 -0500

    Clean Upload

--- app.js
+++ app.js
@@ -12,12 +12,8 @@ const uuid = require('uuid/v4');
 var mainframe = require(__dirname + '/routes/mainframe.js');
 global.rootDir = __dirname;
 
-var jwt=require('jsonwebtoken');
-//https://jsao.io/2015/06/authentication-with-node-js-jwts-and-oracle-database/
-
 const app = express();
 const server=require('http').createServer(app);
-const io = require('socket.io')(server);
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser());
@@ -25,18 +21,7 @@ app.use( express.static( "public" ) );
 
 app.set('views', __dirname+'/views');
 
-app.use(session({
-  genid: (req) => {
-    return uuid() // use UUIDs for session IDs
-  },
-  secret: 'Qu4^}-P]ffsS*UNq>gKyD~G}VHH7%',
-  resave: false,
-  saveUninitialized: true,
-}))
-
 app.use('/', mainframe);
-
-const aUsers= new Set();
 
 const Facility = class{
 	constructor(name,fid, floors){
--- routes/mainframe.js
+++ routes/mainframe.js
@@ -2,9 +2,20 @@ var express = require('express');
 var path = require('path');
 const fs = require('fs');
 const {readdirSync} = require('fs')
-var crypto=require('crypto');
 
 var router = express.Router();
+
+
+const Asset = class{
+	constructor(name,locX,locY,facility){
+		this.name=name;
+		this.x=locX;
+		this.y=locY;
+		this.facility=facility;
+	}
+}
+
+var assets= new Set()
 
 const getDir= source=>
 			readdirSync(source,{withFileTypes: true})
@@ -13,11 +24,11 @@ const getDir= source=>
 
 router.get('/',async(req,res,next)=>{
     try{
-		test= getDir(rootDir + "/public/facilities/")
+		var test= getDir(rootDir + "/public/facilities/")
 		
 		console.log(test)
 		console.log(req.session.userstat);
-		res.render(path.join('./homepage'),{numFac:1, facilities:[{name:"NavarreFSED",fid:"1"}]});
+		res.render(path.join('./homepage'),{numFac:test.length, facilities:test});
 
     }catch (e){
         next(e);
@@ -31,43 +42,61 @@ router.get('/facilities/*',async(req,res,next)=>{
 		
 		console.log(req.session.userstat);
 		if( (req.url).split('/')[3]== "report"){
-			res.render(path.join('./facility/facility'),{title:p});	
+			res.render(path.join('./facility/facility'),{loggedin:req.session.userstat,username:req.session.username,title:p});	
 		}
 		if( (req.url).split('/')[3]== "blueprint"){
-			res.render(path.join('./facility/blueprint'),{title:p});	
+
+			console.log(assets)
+			let tempassets=[]
+			assets.forEach(x=>x.facility===p ? tempassets.push(x):x);
+			console.log(tempassets)
+			res.render(path.join('./facility/blueprint'),{assets:tempassets,title:p});	
 		}
-		res.render(path.join('./facility/facility'),{title:p});
+		res.render(path.join('./facility/facility'),{loggedin:req.session.userstat,username:req.session.username, title:p});
 
     }catch (e){
         next(e);
     }
     
 });
-router.get('/facilities/*/report',async(req,res,next)=>{
-    try{
-		var p=(req.url).split('/')[2];
-		if(req.session.userstat!=1){
-			req.session.userstat=0;
-			req.session.username="";
-		}else{
-			req.session.userstat=1;
-		}
-		console.log(req.session.userstat);
-		res.render(path.join('./chatroom/video'),{loggedin:req.session.userstat,username:req.session.username,title:p});
-
-    }catch (e){
-        next(e);
-    }
-    
-});
-
 
 router.post('/selectFacility',async(req, res,next)=> {
 	try{
+		readinAssets(req.body.facilities, function(data){
+			});
 	res.redirect('/facilities/'+req.body.facilities);		
 	}catch (e){
 		next(e);
 	}
 });
+
+router.post('*/addAsset',async(req, res,next)=> {
+	try{
+		console.log(req.body.dnsname+","+req.body.elX+","+req.body.elY);
+		let asset= new Asset(req.body.dnsname,req.body.elX,req.body.elY,req.body.facility);
+		assets.add(asset);
+		res.redirect('/facilities/'+req.body.facility);		
+	}catch (e){
+		next(e);
+	}
+});
+
+function readinAssets(x,cb){
+	fs.readFile(rootDir+'/public/facilities/'+x+'/assets.json', (err, data) => {  
+                   if (err) throw err;
+                 let fullinfo = JSON.parse(data);
+                  console.log(fullinfo);
+					for (var i = 0; i < fullinfo.length; i++){
+						let asset= new Asset(fullinfo[i].name,fullinfo[i].x,fullinfo[i].y,fullinfo[i].facility)
+						if(assets.has(asset.name)){
+							
+						}else{
+							assets.add(asset)
+						}
+						
+					}
+					cb(fullinfo);
+          });
+}
 
 module.exports = router;
